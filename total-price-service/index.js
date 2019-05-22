ac064ed25bf1fb6b5e3efb3efa3bc61362a81f0d
Will Boyd <willboyd7c2@gmail.com>
2019-05-22 11:55:02 -0400

    add changes

--- total-price-service/index.js
+++ total-price-service/index.js
@@ -3,29 +3,36 @@ var app = express();
 var Request = require("request");
 
 const PORT = process.env.PORT || 3000;
-const FRUIT_HOST = process.env.FRUIT_HOST || "localhost";
-const FRUIT_PORT = process.env.FRUIT_PORT || 8080;
-const VEGETABLE_HOST = process.env.VEGETABLE_HOST || "localhost";
-const VEGETABLE_PORT = process.env.VEGETABLE_PORT || 8081;
+const BASE_PRICE_HOST = process.env.BASE_PRICE_HOST || "localhost";
+const BASE_PRICE_PORT = process.env.BASE_PRICE_PORT || 8080;
+const SALES_HOST = process.env.SALES_HOST || "localhost";
+const SALES_PORT = process.env.SALES_PORT || 8081;
 
 app.get("/", (req, res, next) => {
-  Request.get(`http://${VEGETABLE_HOST}:${VEGETABLE_PORT}`, (error, response, body) => {
+  Request.get(`http://${SALES_HOST}:${SALES_PORT}`, (error, response, body) => {
     if (error) {
       return console.dir(error);
     }
-    var vegetables = JSON.parse(body).vegetables;
-    Request.get(`http://${FRUIT_HOST}:${FRUIT_PORT}`, (error, response, body) => {
+    var sales = JSON.parse(body).products;
+    Request.get(`http://${BASE_PRICE_HOST}:${BASE_PRICE_PORT}`, (error, response, body) => {
       if (error) {
         return console.dir(error);
       }
-      var fruit = JSON.parse(body).fruits;
-      var allProducts = fruit.concat(vegetables).sort();
-      res.json({description: "A list of all produce.", products: allProducts});
+      var basePrices = JSON.parse(body).products;
+      var totalPrices = JSON.parse(JSON.stringify(basePrices));
+      for (const saleItem of sales) {
+        console.log(saleItem.name);
+        var item = totalPrices.find(function(element) {
+          return element.name === saleItem.name
+        })
+        item.price = (item.price - (item.price * (saleItem.discount / 100))).toFixed(2);
+      }
+      res.json({description: "A list of total prices, including discounts.", products: totalPrices});
     });
   });
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(`${PORT} http://${VEGETABLE_HOST}:${VEGETABLE_PORT} http://${FRUIT_HOST}:${FRUIT_PORT}`);
+  console.log(`${PORT} http://${SALES_HOST}:${SALES_PORT} http://${BASE_PRICE_HOST}:${BASE_PRICE_PORT}`);
 });
